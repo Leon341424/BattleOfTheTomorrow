f9bd2b6de832d1a08630f482046ec519e54a6edf
Leon341424 <161507585+Leon341424@users.noreply.github.com>
2025-07-14 13:35:27 -0600

    Movilidad Pulida
    el personaje rota y se controla como normalmente como lo haria en un juego de peleas donde se gira dependiendo la posición del personaje

--- Assets/Project/Scripts/Player1Control.cs
+++ Assets/Project/Scripts/Player1Control.cs
@@ -14,9 +14,10 @@ public class Player1Control : MonoBehaviour
     private bool isJumping = false;
     public float jumpForce;
     private Rigidbody rb;
+    public Transform opponent;
 
-    private float lastPressTime = -1f;
-    private float doubleTapTime = 0.3f;
+    private KeyCode forwardKey;
+    private KeyCode backKey;
 
     void Start()
     {
@@ -24,6 +25,7 @@ public class Player1Control : MonoBehaviour
         originalPosition = transform.position;
         rb = GetComponent<Rigidbody>();
         originalSpeed = speed;
+        rb.constraints = RigidbodyConstraints.FreezePositionZ;
     }
 
     void Update()
@@ -31,33 +33,47 @@ public class Player1Control : MonoBehaviour
         stateInfo = playerAnimator.GetCurrentAnimatorStateInfo(0);
         movX = 0f;
 
+        if (transform.position.x < opponent.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            forwardKey = KeyCode.D;
+            backKey = KeyCode.A;
+        }
+        if (transform.position.x > opponent.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            forwardKey = KeyCode.A;
+            backKey = KeyCode.D;
+        }
+
         bool isDown = Input.GetKey(KeyCode.S);
         playerAnimator.SetBool("down", isDown);
 
         bool isBlock = Input.GetKey(KeyCode.O);
         playerAnimator.SetBool("block", isBlock);
-   
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        bool isWalking = Input.GetKey(KeyCode.D);
 
-        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.LeftShift))
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        bool isWalking = Input.GetKey(forwardKey);
+
+        if (!Input.GetKey(forwardKey) && !Input.GetKey(backKey))
         {
             playerAnimator.SetBool("run", false);
             playerAnimator.SetBool("forward", false);
             speed = originalSpeed;
         }
 
-        if (!Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D) && !isDown && !isBlock)
+        if (!Input.GetKey(KeyCode.LeftShift) && Input.GetKey(forwardKey) && !isDown && !isBlock)
         {
             speed = originalSpeed;
-            movX = 1;
+            //movX = 1;
+            movX = transform.position.x < opponent.position.x ? 1f : -1f;
             playerAnimator.SetBool("forward", isWalking);
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D) && !isDown && !isBlock)
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(forwardKey) && !isDown && !isBlock)
         {
-            speed = originalSpeed * 2;
-            movX = 1;
+            speed = originalSpeed * 2.5f;
+            movX = transform.position.x < opponent.position.x ? 1f : -1f;
             playerAnimator.SetBool("run", isRunning);
         }
         else
@@ -65,15 +81,15 @@ public class Player1Control : MonoBehaviour
             playerAnimator.SetBool("run", false);
         }
 
-        bool isBack = Input.GetKey(KeyCode.A);
+        bool isBack = Input.GetKey(backKey);
         playerAnimator.SetBool("back", isBack);
-        if (Input.GetKey(KeyCode.A) && !isDown && !isBlock)
+        if (Input.GetKey(backKey) && !isDown && !isBlock)
         {
-            movX = -1f;
+            movX = transform.position.x < opponent.position.x ? -1f : 1f;
         }
 
         Vector3 movement = new Vector3(movX, 0f, 0f);
-        rb.linearVelocity = new Vector3(movement.x * speed, rb.linearVelocity.y, rb.linearVelocity.z);
+        rb.linearVelocity = new Vector3(movement.x * speed, rb.linearVelocity.y, 0f);
 
         if (Input.GetKeyDown(KeyCode.W) && !isJumping && !isDown && !isBlock)
         {
@@ -81,6 +97,7 @@ public class Player1Control : MonoBehaviour
             playerAnimator.SetTrigger("jump");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
     }
 
     private void OnCollisionEnter(Collision collision)
